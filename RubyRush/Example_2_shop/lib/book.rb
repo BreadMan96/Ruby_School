ee7043a8ccbe85f1bd3f302d8b1c441ecdfcc05a
BreadMan96 <vlad962706@mail.ru>
2021-09-24 17:56:47 +0300

    Shop v1.0.2

--- RubyRush/Example_2_shop/lib/book.rb
+++ RubyRush/Example_2_shop/lib/book.rb
@@ -2,6 +2,7 @@ class Book < Product
 	attr_reader :name_book, :genre_book, :author_book
 
 	def initialize(params)
+		super
 		@name_book = params[:name_book]
 		@genre_book = params[:genre_book]
 		@author_book = params[:author_book]
--- RubyRush/Example_2_shop/lib/film.rb
+++ RubyRush/Example_2_shop/lib/film.rb
@@ -2,6 +2,7 @@ class Film < Product
 	attr_reader :name_film, :year_film, :director_film
 
 	def initialize(params)
+		super
 		@name_film = params[:name_film]
 		@year_film = params[:year_film]
 		@director_film = params[:director_film]
