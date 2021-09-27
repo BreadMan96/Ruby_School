0143151ddff8b81c2615d4dee9856b41c67c5a05
BreadMan96 <vlad962706@mail.ru>
2021-09-27 14:08:12 +0300

    Improving store functions and optimizing code (Shop v1.1.0)

--- RubyRush/Example_2_shop/shop.rb
+++ RubyRush/Example_2_shop/shop.rb
@@ -2,14 +2,28 @@ require_relative 'lib/product.rb'
 require_relative 'lib/film.rb'
 require_relative 'lib/book.rb'
 
+def print_book(param)
+	param.each do |x,y|
+		puts "Book: #{x.name_book}, Genre: #{x.genre_book}\nAuthor: #{x.author_book}\nPrice: #{x.price} (Amount: #{x.amount})\n#{@separator}"
+		puts
+	end
+end
+
+def print_film(param)
+	param.each do |x, y|
+		puts "Film: #{x.name_film} (#{x.year_film})\nDirector: #{x.director_film}\nPrice: #{x.price} (Amount: #{x.amount})\n#{@separator}"
+		puts	
+	end
+end
+
 films = []
 books = []
+all_products = {
+	Films: films,
+	Books: books
+}
 
-separator = "============================="
-
-product1 = Product.new(price: 250, amount: 4)
-# films << product1
-# puts "Film: Fury\nPrice: #{films[0].price}\n"
+@separator = "============================="
 
 book1 = Book.new(price: 1000, amount: 10, name_book: "Good to Great", genre_book: "Business", author_book: "Jim Collins")
 book2 = Book.new(price: 750, amount: 10, name_book: "Habits of a Happy Brain", genre_book: "Psychology", author_book: "Loretta Graziano Breuning")
@@ -17,11 +31,28 @@ books << book1
 books << book2
 
 film1 = Film.new(price: 200, amount: 100, name_film: "Fury", year_film: 2014, film_director: "David Ayer")
-film2 = Film.new(price: 450, amount: 100, name_film: "Avengers: Endgame", year_film: 2019, film_director: "Anthony Russo, Joe Russo")
+film2 = Film.new(price: 450, amount: 100, name_film: "Avengers: Endgame", year_film: 2019, director_film: "Anthony Russo, Joe Russo")
 films << film1
 films << film2
 
-books.each_with_index do |x, y|
-	puts "Book: #{x.name_book}, Genre: #{x.genre_book}\nAuthor: #{x.author_book}\nPrice: #{x.price} (Amount: #{x.amount})\n#{separator}"
-	puts	
+print "Choose a product category:\n1. Books\n2. Films\n3.Show all\nEnter number (1,2 or 3): "
+category = gets.strip.to_i
+
+puts @separator
+
+case category
+when 1
+	print_book(books)
+when 2
+	print_film(films)
+when 3
+	all_products.each do |key,value|
+		puts key.upcase
+		puts @separator
+		if key == :Films
+			print_film(value)
+		elsif key == :Books
+			print_book(value)
+		end
+	end
 end
