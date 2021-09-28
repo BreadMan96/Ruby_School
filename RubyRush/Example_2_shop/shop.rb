639d23d41921cb4afd4fe75c627b37ec435678bd
BreadMan96 <vlad962706@mail.ru>
2021-09-28 15:37:04 +0300

    New version Shop (v1.4.0) - add promocode, sort, fix bugs

--- RubyRush/Example_2_shop/shop.rb
+++ RubyRush/Example_2_shop/shop.rb
@@ -3,20 +3,19 @@ require_relative 'lib/film.rb'
 require_relative 'lib/book.rb'
 
 
-# collection.sort!(by: :price, order: :asc)
 def sort(param, what_sorting)
 	case param
 	when 1
-	  what_sorting.sort_by! { |product| product.price }
+	  what_sorting.sort_by! { |product| product.price } # цена по возрастанию
 	when 2
-	  what_sorting.sort_by! { |product| product.amount }
+	  what_sorting.sort_by! { |product| product.amount } # кол-во по возрастанию
 	end
 end
 
 def print_book(param)
-	sort(2, param)
+	sort(1, param)
 	param.each do |x,y|
-		puts "Book: #{x.name_book}, Genre: #{x.genre_book}\nAuthor: #{x.author_book}\nPrice: #{x.price} (Amount: #{x.amount})\n#{@separator}"
+		puts "Book: #{x.name_book} | Genre: #{x.genre_book}\nAuthor: #{x.author_book}\nPrice: #{x.price} (Amount: #{x.amount})\n#{@separator}"
 		puts
 	end
 end
@@ -36,28 +35,37 @@ all_products = {
 	Films: films,
 	Books: books
 }
+promocods = {'BREADMAN' => 50, 'BUSINESS' => 15, 'NHL2022' => 22, 'RUBY' => 25}
 
 @separator = "============================="
 
-book1 = Book.new(price: 1000, amount: 10, name_book: "Good to Great", genre_book: "Business", author_book: "Jim Collins")
-book2 = Book.new(price: 750, amount: 10, name_book: "Habits of a Happy Brain", genre_book: "Psychology", author_book: "Loretta Graziano Breuning")
-book1.price = 150
-books << book1
-books << book2
+book1 = Book.new(price: 1000, amount: 100, name_book: "Good to Great", genre_book: "Business", author_book: "Jim Collins")
+book2 = Book.new(price: 7050, amount: 10, name_book: "Habits of a Happy Brain", genre_book: "Psychology", author_book: "Loretta Graziano Breuning")
+#book1.price = 1500 - возможность менять цену
+books.push(book1,book2)
 
 film1 = Film.new(price: 200, amount: 100, name_film: "Fury", year_film: 2014, director_film: "David Ayer")
-film2 = Film.new(price: 450, amount: 10, name_film: "Avengers: Endgame", year_film: 2019, director_film: "Anthony Russo, Joe Russo")
-films << film1
-films << film2
-
-#@books.sort_by! { |product| product.price }   # рабочая
-
-#sort(1, @books)
+film2 = Film.new(price: 450, amount: 1000, name_film: "Avengers: Endgame", year_film: 2019, director_film: "Anthony Russo, Joe Russo")
+films.push(film1,film2)
 
 print "Choose a product category:\n1. Books\n2. Films\n3. Show all\nEnter number (1,2 or 3): "
 category = gets.strip.to_i
 
+print "Do you have a promocode?(if yes, enter it): "
+promocod = gets.strip.upcase
+
+if promocods.include?(promocod)
+	all_products.each do |key, value|
+		value.each do |x|
+			x.price = x.price - (x.price * promocods[promocod] / 100)
+		end
+	end
+	puts "Promocode '#{promocod}' has been successfully applied! Your promocode discount: #{promocods[promocod]}%"
+end
+
 puts @separator
+
+sleep 2
 
 case category
 when 1
